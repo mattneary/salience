651823ccf08e909c97128a7ba04e29cd41c50f85
fitsum <fitsumbelay@gmail.com>
2023-07-19 20:24:50 -0400

    wrap range input listener in function + add listener to link in 'splash screen' that adds UI after user click + add mutation observer to reveal 'splash screen' after content loads

--- salience/static/ui.js
+++ salience/static/ui.js
@@ -2,7 +2,31 @@
   const $ = selector => document.querySelector( selector )
   const $$ = selector => document.querySelectorAll( selector ) 
 
-  const rangeInput = $('input[type="range"')
+  const uiContainer = $('#ui-container')
+
+  const revealUI = (changes, observer) =>{
+    uiContainer.classList = ''   
+    observer.disconnect()
+  }
+  const content = $('#content')
+  const observerConfig = { childList: true  }
+  const contentObserver = new MutationObserver(revealUI)
+  contentObserver.observe(content, observerConfig)
+
+  const uiString = '<input type="range" list="markers" value="0" min="0" max="1" step="0.01"  /><span id="rangeValue">0</span>'
+  const hidePerSalienceUI = new DOMParser().parseFromString(uiString, 'text/html').body.innerHTML
+
+
+  const addUIListener = () => {
+    const rangeInput = $('input[type="range"')
+    rangeInput.addEventListener('input', e => {
+      const v = e.target.value
+      const sen = $$( '.sentence' )
+      showAllSentences( sen )
+      filterSentences( sen , v )
+      $('#rangeValue').textContent = v
+    })
+  }
 
   const showAllSentences = ( sentences ) => sentences.forEach( s => s.style.opacity = 1 )
 
@@ -16,11 +40,10 @@
   }
 
 
-  rangeInput.addEventListener('input', e => {
-    const v = e.target.value
-    const sen = $$( '.sentence' )
-    showAllSentences( sen )
-    filterSentences( sen , v )
-    $('#rangeValue').textContent = v
+  $('#ui-container a').addEventListener('click', e => {
+   e.preventDefault()
+   e.target.parentNode.innerHTML = hidePerSalienceUI
+   addUIListener()
   })
+
 })()
